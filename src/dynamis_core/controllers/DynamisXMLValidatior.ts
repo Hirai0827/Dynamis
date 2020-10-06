8c3f16a133b5cf4ec2f0b185404f1cdbb0ca7aa0
hirai <ryu1999827@gmail.com>
2020-10-07 01:59:01 +0900

    remove ignore

--- src/dynamis_core/controllers/DynamisXMLValidatior.ts
+++ src/dynamis_core/controllers/DynamisXMLValidatior.ts
@@ -2,7 +2,6 @@ export class DynamisXMLValidatior {
     public static Validate:(str:string) => boolean = (str:string) => {
         const pointAngle = /(>|<)/g;
         let isInBrackets:boolean = false;
-        // @ts-ignore
         const arr:Array<RegExpMatchArray> = Array.from(str.matchAll(pointAngle));
         for(let i = 0; i < arr.length; i++){
             const char = arr[i][0];
