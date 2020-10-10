fdc5cb612754f3e0a4418f270e32cb2b144277a7
hirai <ryu1999827@gmail.com>
2020-10-11 03:44:25 +0900

    add: scale node

---
+++ src/dynamis_core/datas/DynamisNodes/DynamisScaleNode.ts
@@ -0,0 +1,33 @@
+import {IDynamisNode} from "../IDynamisNode";
+import {CodeGenerateProps, DynamisNodeParams} from "../DynamisNodeData";
+import {DynamisNameProvider} from "../../controllers/DynamisNameProvider";
+
+export class DynamisScaleNode implements IDynamisNode {
+    child: Array<IDynamisNode> = new Array<IDynamisNode>();
+    generateCode = () => {
+        const posName = DynamisNameProvider.GetPosValName(this.props.posId);
+        const parentPosName = DynamisNameProvider.GetPosValName(this.parent ? this.parent.props.posId : -1);
+        let scale:string = this.params["scale"] ? this.params["scale"] : "1.0";
+        let str = `vec3 ${posName} = ${parentPosName};\n`;
+        str += `${posName} = ${parentPosName} / ${scale};\n`;
+        for(let i = 0; i < this.child.length; i++){
+            const codeGenerateResult = this.child[i].generateCode();
+            str += codeGenerateResult;
+        }
+        return str;
+    };
+    params: DynamisNodeParams = {};
+    parent: IDynamisNode | null = null;
+    allocateProps: (codeGenerateProps: CodeGenerateProps) => CodeGenerateProps = codeGenerateProps => {
+        codeGenerateProps.posId++;
+        this.props.posId = codeGenerateProps.posId;
+        this.props.distId = this.parent ? this.parent.props.distId : -1;
+        for(let i = 0; i < this.child.length; i++){
+            codeGenerateProps = this.child[i].allocateProps(codeGenerateProps);
+        }
+        return codeGenerateProps;
+    };
+    props: CodeGenerateProps = {posId:-1,distId:-1};
+
+
+}
