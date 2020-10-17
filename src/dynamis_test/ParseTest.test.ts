303979dff03df2a582ef8e4636bd172e9b9f0e7b
hirai <ryu1999827@gmail.com>
2020-10-17 22:49:54 +0900

    add: comment out syntax

--- src/dynamis_test/ParseTest.test.ts
+++ src/dynamis_test/ParseTest.test.ts
@@ -19,14 +19,31 @@ test("ParseTest",() => {
     //存在しないタグ
     res = DynamisCompiler.Compile("<render><hogehoge/></render>");
     expect(res.errorType).toBe("UnexpectedNodeError");
+});
+
+test("ParseTestWithParams",() => {
+    //Paramに関する要素が含まれたテスト
     //OK パラメータ付き
-    res = DynamisCompiler.Compile("<render><sphere radius='12.0'/></render>");
+    let res = DynamisCompiler.Compile("<render><sphere radius='12.0'/></render>");
     expect(res.state).toBe("success");
     //パラメータ不正
-    res = DynamisCompiler.Compile("<render><hogehoge radius='12.0' hoge/></render>");
+    res = DynamisCompiler.Compile("<render><sphere radius='12.0' hoge/></render>");
     expect(res.errorType).toBe("ParseError");
+    //パラメータ不正
+    res = DynamisCompiler.Compile("<render><sphere radius!='12.0'/></render>");
+    expect(res.errorType).toBe("ParseError");
+    //OK パラメータ付き
+    res = DynamisCompiler.Compile("<render><sphere radius = '12.0'/></render>");
+    expect(res.state).toBe("success");
+    //OK パラメータ付き
+    res = DynamisCompiler.Compile("<render><box x = '12.0' y = '12.0'/></render>");
+    expect(res.state).toBe("success");
+});
+
+test("ParseTestWithNoise",() => {
+   //構文には関係ない要素が含まれたテスト
     //非タグの挿入
-    res = DynamisCompiler.Compile("<render><sphere/>aaa</render>");
+    let res = DynamisCompiler.Compile("<render><sphere/>aaa</render>");
     expect(res.state).toBe("failed");
     //改行の挿入
     res = DynamisCompiler.Compile(`\n<render><sphere/>
@@ -38,10 +55,41 @@ test("ParseTest",() => {
     //タブの挿入
     res = DynamisCompiler.Compile(`<render>\t   <sphere/>\n</render>`);
     expect(res.state).toBe("success");
-    //OK パラメータ付き
-    res = DynamisCompiler.Compile("<render><sphere radius = '12.0'/></render>");
+    //コメントの挿入
+    res = DynamisCompiler.Compile(`<render>/**/<sphere/></render>`);
     expect(res.state).toBe("success");
-    //OK パラメータ付き
-    res = DynamisCompiler.Compile("<render><box x = '12.0' y = '12.0'/></render>");
+    //コメントの挿入
+    res = DynamisCompiler.Compile(`<render>/*hogehogehugahuga*/<sphere/></render>`);
     expect(res.state).toBe("success");
+    //コメント数不全
+    res = DynamisCompiler.Compile(`<render>/*/*hogehogehugahuga*/<sphere/></render>`);
+    expect(res.state).toBe("success");
+    //コメント数不全
+    res = DynamisCompiler.Compile(`<render>/*hogehogehugahuga*/*/<sphere/></render>`);
+    expect(res.state).toBe("success");
+    //コメント数不全
+    res = DynamisCompiler.Compile(`<render>/*/<sphere/></render>`);
+    expect(res.state).toBe("failed");
+    //コメントのスラッシュ逆
+    //コメント数不全
+    res = DynamisCompiler.Compile(`<render>/*/*hogehogehugahuga*/*/<sphere/></render>`);
+    expect(res.state).toBe("success");
+    //コメントのスラッシュ逆
+    res = DynamisCompiler.Compile(`<render>\**\<sphere/></render>`);
+    expect(res.state).toBe("failed");
+    //コメント閉じてない
+    res = DynamisCompiler.Compile(`<render>\*<sphere/></render>`);
+    expect(res.state).toBe("failed");
+    //コメント閉じてない
+    res = DynamisCompiler.Compile(`<render><sphere/></render>\*`);
+    expect(res.state).toBe("failed");
+    //コメント閉じてない
+    res = DynamisCompiler.Compile(`<render><sphere/></render>*\ `);
+    expect(res.state).toBe("failed");
+    //Tag in Comment
+    res = DynamisCompiler.Compile(`<render><sphere/>\*</render>*\ `);
+    expect(res.state).toBe("failed");
+    //Comment in Tag
+    res = DynamisCompiler.Compile(`<render><sphere/></render\*hoge*\> `);
+    expect(res.state).toBe("failed");
 });
