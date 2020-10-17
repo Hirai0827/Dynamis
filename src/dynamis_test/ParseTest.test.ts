2071ae230012c3b820be833a9fb5095e37ce8a82
hirai <ryu1999827@gmail.com>
2020-10-17 16:24:23 +0900

    add: parse test

--- src/dynamis_test/ParseTest.test.ts
+++ src/dynamis_test/ParseTest.test.ts
@@ -1,10 +1,22 @@
 import {DynamisCompiler} from "../dynamis_core/controllers/DynamisCompiler";
 
 test("ParseTest",() => {
+    //開始タグのみ
     let res = DynamisCompiler.Compile("<render>");
     expect(res.state).toBe("failed");
-    res = DynamisCompiler.Compile("<render></render>");
-    expect(res.state).toBe("success");
+    //タグ不一致
     res = DynamisCompiler.Compile("<render></render1>");
     expect(res.state).toBe("failed");
+    //OK
+    res = DynamisCompiler.Compile("<render></render>");
+    expect(res.state).toBe("success");
+    //空タグになっていない
+    res = DynamisCompiler.Compile("<render><sphere></render>");
+    expect(res.state).toBe("failed");
+    //OK
+    res = DynamisCompiler.Compile("<render><sphere/></render>");
+    expect(res.state).toBe("success");
+    //存在しないタグ
+    res = DynamisCompiler.Compile("<render><hogehoge/></render>");
+    expect(res.errorType).toBe("UnexpectedNodeError");
 });
